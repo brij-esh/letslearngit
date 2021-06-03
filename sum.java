526f6875a2e996a3018b368b12b131ab921e8ec8
Brijesh yadav <byadav2751@gmail.com>
2021-06-03 14:51:36 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println(a + b);
+        System.out.println("sum" + a + b);
 
     }
 }
