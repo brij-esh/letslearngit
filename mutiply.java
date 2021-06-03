dd8f48e8fb7eaa74ffb72540a9cba6ebb7ba3940
Brijesh yadav <byadav2751@gmail.com>
2021-06-03 15:01:37 +0530

    adding multiply.java

---
+++ mutiply.java
@@ -0,0 +1,10 @@
+public class mutiply {
+    // public class sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("sum" + a + b);
+
+        // }
+    }
+}
